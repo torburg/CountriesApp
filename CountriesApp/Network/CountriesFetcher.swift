9fc02c645815839669e44924f79a13853ae68e20
torburg <torburg@yandex.ru>
2020-04-25 18:17:09 +0300

    Init commit with Fetcher, DataManager, TableView and gitignore created

---
+++ CountriesApp/Network/CountriesFetcher.swift
@@ -0,0 +1,9 @@
+//
+//  CountriesFetcher.swift
+//  CountriesApp
+//
+//  Created by Maksim Torburg on 25.04.2020.
+//  Copyright © 2020 Maksim Torburg. All rights reserved.
+//
+
+import Foundation
---
+++ CountriesApp/Storage/CoreDataManager.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataManager.swift
+//  CountriesApp
+//
+//  Created by Maksim Torburg on 25.04.2020.
+//  Copyright © 2020 Maksim Torburg. All rights reserved.
+//
+
+import Foundation
+
+class CoreDataManager {
+    static var instance = CoreDataManager()
+    
+}
